e0bfa2f710dab8db98bb61dd81c431b3b80f9912
Kostiantyn Siharov <ksigaryov@gmail.com>
2023-10-24 21:58:00 +0200

    Refactoring HTTP responses

--- app/controllers/article_controller.rb
+++ app/controllers/article_controller.rb
@@ -3,73 +3,88 @@
 require_relative 'application_controller'
 require_relative '../helpers/articles_helper'
 require_relative '../models/article'
-require 'pry'
+
 class ArticleController < ApplicationController
   configure do
     helpers ArticlesHelper
   end
 
-  # @method: Display a small welcome message
-  get '/hello' do
-    "The server is up and running! The current time: #{server_time_run}"
+  # @method: Display all articles
+  get '/articles/?' do
+    type = accepted_media_type
+
+    if type == 'json'
+      content_type 'application/json'
+      json_response(response_message(find_articles))
+    elsif type == 'xml'
+      content_type 'application/xml'
+      Gyoku.xml(articles: response_message(find_articles))
+    end
+  rescue StandardError
+    halt 422, 'Something wrong!'
   end
 
-  # @method: Display all articles in json
-  # $curl 127.0.0.1:9292/articles_json
-  get '/articles_json/?' do
-    articles = find_articles
-    if articles.length.positive?
-      json_response(data: articles)
-    else
-      json_response(data: 'No articles have been created yet!')
+  # @method: Display the article
+  get '/articles/:id/?' do
+    type = accepted_media_type
+
+    if type == 'json'
+      content_type 'application/json'
+      json_response(data: find_article)
+    elsif type == 'xml'
+      content_type 'application/xml'
+      Gyoku.xml(article: find_article)
+    end
+  rescue StandardError
+    halt 422, 'Something wrong!'
+  end
+
+  # @method: Receiving back information from the GET request by the client
+  head '/articles/?' do
+    type = accepted_media_type
+
+    if type == 'json'
+      content_type 'application/json'
+    elsif type == 'xml'
+      content_type 'application/xml'
     end
   end
 
   # @method: Add a new article to the DB
-  # $curl -X POST 127.0.0.1:9292/articles_json/create -d '{}'
-  # $curl -X POST 127.0.0.1:9292/articles_json/create -d \
-  # '{"id":8,"title":"Article","content":"This is new article","autor":"Autor"}'
-  post '/articles_json/create/?' do
+  post '/articles/?' do
     article = Article.create(data_json(created: true))
+    url = "http://localhost:9292/articles/#{article[:id]}"
+    response.headers['Location'] = url
+    status 201
 
     if sinatra_flash_error(article).length.positive?
-      json_response_db(data: article, message: sinatra_flash_error(article))
-    else
-      json_response_db(data: article, message: 'Current article')
+      json_response(data: article)
+      json_response(data: sinatra_flash_error(article))
     end
-  rescue StandardError => e
-    json_response(code: 422, data: { error: e.message })
-  end
-
-  # @method: Display the article in json
-  get '/articles_json/:id/?' do
-    json_response(data: find_article)
-  rescue StandardError => e
-    json_response(code: 422, data: { error: e.message })
+  rescue StandardError
+    halt 422, 'Something wrong!'
   end
 
   # @method: Update the article in the DB according to :id
-  # curl -X PUT 127.0.0.1:9292/articles_json/1/edit -d '{}'
-  put '/articles_json/:id/edit/?' do
+  put '/articles/:id/edit/?' do
     article = Article.find(article_id)
     article.update(data_json)
+    status article ? 204 : 201
 
     if sinatra_flash_error(article).length.positive?
-      json_response_db(data: article, message: sinatra_flash_error(article))
-    else
-      json_response_db(data: article, message: 'Current article')
+      json_response(data: article)
+      json_response(data: sinatra_flash_error(article))
     end
-  rescue StandardError => e
-    json_response(code: 422, data: { error: e.message })
+  rescue StandardError
+    halt 422, 'Something wrong!'
   end
 
   # @method: Delete the article in the DB according to :id
-  # curl -X DELETE 127.0.0.1:9292/articles_json/15/destroy
-  delete '/articles_json/:id/destroy/?' do
+  delete '/articles/:id/destroy/?' do
     article = Article.find(article_id)
     article.destroy
-    json_response(data: { message: 'Article deleted successfully!' })
-  rescue StandardError => e
-    json_response(code: 422, data: { error: e.message })
+    status 204
+  rescue StandardError
+    halt 422, 'Something wrong!'
   end
 end
