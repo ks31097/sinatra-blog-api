728704c8ac8eac62ec8ca15d21907ff13de2a5bb
Kostiantyn Siharov <ksigaryov@gmail.com>
2023-10-24 22:00:46 +0200

    Updated methods

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,30 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
+  def json_or_default?(type)
+    ['application/json', 'application/*', '*/*'].include?(type.to_s)
+  end
+
+  def xml?(type)
+    type.to_s == 'application/xml'
+  end
+
+  def accepted_media_type
+    return 'json' unless request.accept.any?
+
+    request.accept.each do |type|
+      return 'json' if json_or_default?(type)
+      return 'xml' if xml?(type)
+    end
+
+    halt 406, 'Not Acceptable'
+  end
+
+  # @api: Format the json response
+  def json_response(message)
+    { data: message }.to_json
+  end
+
   def server_time_run
     DateTime.now.strftime('%d-%m-%Y %H:%M')
   end
--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -3,12 +3,20 @@
 module ArticlesHelper
   # @find all articles
   def find_articles
-    Article.all
+    Article.all.as_json
   end
 
   # @find the article
   def find_article
-    Article.find(article_id)
+    Article.find(article_id).as_json
+  end
+
+  def response_message(data_message)
+    if data_message.length.positive?
+      data_message
+    else
+      'No articles have been created yet!'
+    end
   end
 
   private
@@ -17,7 +25,7 @@ module ArticlesHelper
   def data_json(created: false)
     payload = JSON.parse request.body.read
     if created
-      payload['created_at'] = time_now
+      payload['created_at'] ||= time_now
       payload['updated_at'] = time_now
     end
     payload
